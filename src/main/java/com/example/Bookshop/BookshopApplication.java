6b6a9bfd95f9f41a54f9bde7f96a60b56f6d7f2b
Cezary Rasiński <134779981+cezary-rasinski@users.noreply.github.com>
2025-07-05 17:08:52 +0200

    first commit

---
+++ src/main/java/com/example/Bookshop/BookshopApplication.java
@@ -0,0 +1,14 @@
+package com.example.Bookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshopApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
